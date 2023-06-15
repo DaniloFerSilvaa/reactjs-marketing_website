e0ddf55f8fd0c99e21139fe2c2f9bc8d367858ef
Danilo Silva <danilofernandessilva98@gmail.com>
2023-06-15 00:23:23 -0300

    atualizado tela de contactus

--- src/pages/ContactUs/ContactUs.tsx
+++ src/pages/ContactUs/ContactUs.tsx
@@ -1,7 +1,27 @@
+import { Logo } from '../../components/logoSvg/logo';
+import styles from './ContactUs.module.css';
 
 export const ContactUs = () => {
-
+    
+    
      return(
-          <div>contact us</div>
-     )
+          <main className={styles.mainContact}>
+               <div className={styles.bgUp}>
+                    
+               </div>
+               <div className={styles.bgDown}></div>
+               <div className={styles.container}>
+                    <div className={styles.logoContact}>
+                         <Logo/> <h1>Lorem</h1>
+                    </div>
+                    <div className={styles.generalInfoArea}>
+                         <div className={styles.textInfo}></div>
+                         <div className={styles.iconArea}></div>
+                         <div className={styles.formArea}>
+                              
+                         </div>
+                    </div>
+               </div>
+          </main>
+    )
 }
